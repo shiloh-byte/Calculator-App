d787ebaa2049c24db9f753a54ab10556be8fead8
shiloh-byte <57815430+shiloh-byte@users.noreply.github.com>
2022-05-28 20:22:31 -0500

    Update app.js
    Changed division symbol for case ' ' in compute().

--- app.js
+++ app.js
@@ -46,7 +46,7 @@ class Calculator {
           case 'x':
             computation = prev * current
             break
-          case '÷':
+          case '/':
             computation = prev / current
             break
           default:
